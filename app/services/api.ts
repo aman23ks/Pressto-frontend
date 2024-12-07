cfda70d5f98629ffbff9cf03289a40ecdf24d108
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2024-12-08 01:15:31 +0530

    backend api fix

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://pressto-backend.vercel.app/api';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://pressto-backend.vercel.app/api';
+// const API_URL = 'http://localhost:5000/api';
 
 const apiService = axios.create({
   baseURL: API_URL,
