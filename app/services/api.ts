9ca762304a7d7bdec69f35dc5cdff10d18fc270b
amanshrivastava23 <aman.shrivastava@kommunicate.io>
2024-12-11 23:20:50 +0530

    updated backend api

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://pressto-backend.vercel.app/api";
 
 const apiService = axios.create({
   baseURL: API_URL,
